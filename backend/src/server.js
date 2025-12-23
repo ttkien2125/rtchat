621b26f454a9e357fefb124b87cd921c7f4a3b7e
ttkien2125 <imgoodatgamesm8@gmail.com>
2025-12-23 14:08:04 +0700

    Deployment test

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -7,10 +8,19 @@ import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    const filePath = path.join(__dirname, "../frontend/dist/index.html");
+    app.get("*", (req, res) => res.sendFile(filePath));
+}
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
